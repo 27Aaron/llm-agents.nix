d0c7856fd4af4ba8efe0b047188936aa65388ed8
Jörg Thalheim <joerg@thalheim.io>
2026-08-20 07:57:13 +0200

    luvus: skip flaky pty tests
    They spawn real processes/PTYs and race under CI load; failures
    differ per platform and run.

--- packages/luvus/package.nix
+++ packages/luvus/package.nix
@@ -50,6 +50,15 @@ rustPlatform.buildRustPackage (finalAttrs: {
     git
   ];
 
+  # flaky: spawn real PTYs/processes and race under load
+  checkFlags = [
+    "--skip=app::tests::clicking_a_pane_title_shows_the_real_command"
+    "--skip=app::tests::keyboard_copy_mode_yanks_history_and_cancel_restores_its_viewport"
+    "--skip=app::tests::resize_yields_to_pane_title_and_zoom_but_still_grabs_the_seam"
+    "--skip=app::tests::resume_session_opens_pane"
+    "--skip=platform::tests::process_tree_finds_this_process_and_its_children"
+  ];
+
   postFixup = ''
     wrapProgram $out/bin/luvus \
       --prefix PATH : ${lib.makeBinPath runtimeTools}
